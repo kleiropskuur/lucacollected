40e74e7371287a98f8480953e5e6d6efc07950c0
kleiropskuur <kleiropskuur@gmail.com>
2023-01-22 21:00:26 +0100

    1

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 document.querySelector('.menubtn').onclick = function (e) {
     var menubtn = document.querySelector('.menubtn');
     var menu = document.querySelector('.menu');
+    var header = document.querySelector('.header')
+    var headermark = document.querySelector('.headermark')
   
     menubtn.classList.toggle('is-active');
     menu.classList.toggle('is-active');
+    header.classList.toggle('is-active');
+    headermark.classList.toggle('is-active');
   
     e.preventDefault();
   }
