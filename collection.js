aa7248b02844778647877f6adfe6b861e9fd6409
kleiropskuur <kleiropskuur@gmail.com>
2023-01-23 15:57:34 +0100

    1

---
+++ collection.js
@@ -0,0 +1,19 @@
+var options = {
+    valueNames: [ 'title', 'author', 'publisher', 'date' ]
+  };
+
+  var values = [
+    {
+      title: 'Sense',
+      author: 'The Lightning Seeds',
+      publisher: 'check',
+      date: 1992
+    },
+    {
+      title: "His 'N' Hers",
+      author: 'Pulp',
+      publisher: 'crack',
+      date: 1994
+    }
+  ];
+  
